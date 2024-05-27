3947deee575b88f4005dc61e210f206f9b814199
yasmine <yasmine.hikal@gmail.com>
2024-05-27 23:37:48 +0200

    search in rooms

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Room;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 use Illuminate\View\View;
@@ -12,9 +13,15 @@ class RoomController extends Controller
     /**
      * Display rooms listing.
      */
-    public function index(): View
+    public function index(Request $request): View
     {
-        $rooms =  Room::paginate(10);
+        $rooms =  Room::query()
+            ->when(
+                $request->q,
+                function (Builder $builder) use ($request) {
+                    $builder->where('name', 'like', "%{$request->q}%");
+                }
+            )->paginate(10);
         return view('rooms.index', [
             'rooms' => $rooms,
         ]);
--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -5,7 +5,7 @@
             {{ __('Rooms') }}
         </h2>
     </x-slot>
-{{--    @include('sweetalert::alert')--}}
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
@@ -15,7 +15,18 @@
                     <div class="card-header dark:bg-gray-900 text-white">
                         <h3>{{__('Rooms listing')}}</h3>
                     </div>
+
                     <div class="card-body">
+                        <form action="{{ route('room.index') }}" method="get">
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control mb-3" placeholder="search" name="q" value="{{request()->q}}">
+                                </div>
+                                <div class="col-md-2">
+                                    <input type="submit" class="form-control mb-3" value="Search">
+                                </div>
+                            </div>
+                        </form>
                         <table class="table table-striped table-hover">
                             <thead>
                             <tr>
